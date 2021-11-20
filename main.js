cfd7a20986ffd1b456dfd1125484f293768b9ff6
dobrovolskyv <malak3@mail.ru>
2021-11-20 13:45:16 +0300

    add scrollbar

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let progress = document.querySelector("#progressbar");
 function imgSlider(anything) {
   document.querySelector(".coffe__img").src = anything;
 }
@@ -6,3 +7,10 @@ function changeCircleColor(color) {
   const circle = document.querySelector(".circle");
   circle.style.backgroundColor = color;
 }
+
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.addEventListener("scroll", function () {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + "%";
+});
